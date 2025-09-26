6f2395cbf7053a57f0f9ff6b611a95efb80c1b6b
jkoigiweb-12 <jkoigi12@gmail.com>
2025-09-26 12:01:44 +0300

    fourth commit

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,14 +1,7 @@
 <script setup>
-
-const menuItem={
-    
-        name: 'fries',
-        image:"/images/menu/robby-mccullough-HNObIv6zmN0-unsplash.jpg",
-        price: 950,
-        description: ' Ultra-thin and crispy.',
-        category: 'Main course',
-    
-}
+import { useMenuStore } from '../stores/menu';
+const menuStore = useMenuStore()
+const menuItem=menuStore.selectedMenuItem
 </script>
 
 <template>
@@ -19,14 +12,41 @@ const menuItem={
                 <v-img :src="menuItem.image" cover height="500" ></v-img>
             </v-col>
             <v-col md="6">
-                <v-card class="pa-6 rounded-lg" justify="center"></v-card>
+                <v-card class="pa-6 rounded-lg" justify="center">
                 <v-card-title>{{menuItem.name}}</v-card-title>
                 <v-card-subtitle>{{ menuItem.price }}</v-card-subtitle>
                 <v-card-text>{{ menuItem.description }}</v-card-text>
-                <v-card-text> Quantity <v-text-field></v-text-field></v-card-text>
+                <v-row>
+                    <v-col md="2"><v-card text="">Quantity</v-card text></v-col>
+                        <v-col md="10">
+                            <v-number-input
+                               density="compact"
+                                :min="1"
+                                :max="10">
+                            </v-number-input>
+                        </v-col>
+                </v-row>
                 <v-card-actions>
-                    <v-btn color="light-blue-lighten-1">Order</v-btn>
+                    <v-btn color="light-blue-lighten-1" variant="elevated">Order</v-btn>
                 </v-card-actions>
+                </v-card>
+                <v-card class="pa-6 mt-3 rounded-lg" justify="center">
+                    <v-row>
+                        <v-card-title>Reviews</v-card-title>
+                        <v-rating    
+                            hover
+                            :length="5"
+                            :size="40"
+                            :model-value="5"
+                            active color="#light-blue-lighten-1">
+
+                        </v-rating>
+                    
+                        
+                        
+                       
+                    </v-row>
+                </v-card>
             </v-col>
         </v-row>
     </v-container>
